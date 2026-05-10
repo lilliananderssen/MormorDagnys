7d5ab1ac626507c5ba163c3d2fd2920377fc9a25
lilliananderssen <lillianderssen@gmail.com>
2026-05-10 21:14:29 +0200

    Ny struktur

--- DTOs/Products/PatchPriceDto.cs
+++
@@ -1,7 +0,0 @@
-namespace MormorDagnys.DTOs.Products;
-
-// Tar emot ett nytt pris från klienten för att uppdatera vad en leverantör tar för en specifik produkt
-public class PatchPriceDto
-{
-    public decimal PricePerKg { get; set; }
-}
--- DTOs/Suppliers/PostSupplierProductDto.cs
+++
@@ -1,8 +0,0 @@
-namespace MormorDagnys.DTOs.Suppliers;
-
-// Tar emot produkt och pris från klienten för att lägga till en ny produkt hos en leverantör
-public class PostSupplierProductDto
-{
-    public int ProductId { get; set; }
-    public decimal PricePerKg { get; set; }
-}
